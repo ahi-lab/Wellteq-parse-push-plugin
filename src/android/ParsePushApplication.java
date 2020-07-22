2d2f382387d06f5921ebf37dc577f4a71c38f79e
Pushparaj <pushparaj@wellteq.co>
2020-07-23 01:54:24 +0800

    Update ParsePushApplication.java

--- src/android/ParsePushApplication.java
+++ src/android/ParsePushApplication.java
@@ -2,6 +2,8 @@ package github.taivo.parsepushplugin;
 
 import android.app.Application;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.messaging.FirebaseMessaging;
 import com.parse.Parse;
 import com.parse.Parse.Configuration.Builder;
 import com.parse.ParseInstallation;
@@ -13,13 +15,20 @@ import github.taivo.parsepushplugin.ParsePushConfigException;
 
 import android.util.Log;
 
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
+import com.parse.fcm.ParseFCM;
+
 /*
    Why is this Application subclass needed?
       - Cordova does not define an Application class, only Activity.
       - The android cold start sequence is: create Application -> ... --> handle push --> ... -> launch Activity,
       - Without configuring an Application class, the app would crash during push notification cold start because
          Parse.Push is not initialized before the "handle push" phase.
-
    How does Android know to use this subclass as the main application class?
       - In AndroidManifest.xml, the <application> class has an attribute "android:name" that points to your designated main application class.
       - This plugin automatically sets android:name during plugin installation IFF it doesn't exist.
@@ -28,67 +37,68 @@ import android.util.Log;
          i.e., android:name="com.custom.package.MainApplication"
 */
 public class ParsePushApplication extends Application {
-  public static final String LOGTAG = "ParsePushApplication";
+    public static final String LOGTAG = "ParsePushApplication";
 
-  @Override
-  public void onCreate() {
-    super.onCreate();
+    @Override
+    public void onCreate() {
+        super.onCreate();
 
-    try {
-      // Other ways to call ParsePushReaderConfig:
-      //
-      // - Tell the reader to parse custom parameters, e.g., <preference name="CustomParam1" value="foo" />
-      //   ParsePushConfigReader config = new ParsePushConfigReader(getApplicationContext(), null, new String[] {"CustomParam1", "CustomParam2"});
-      //
-      // - If you write your own MainApplication in your app package, just import com.yourpackage.R and skip detecting R.xml.config
-      //   ParsePushConfigReader config = new ParsePushConfigReader(getApplicationContext(), R.xml.config, null);
-      //
+        try {
 
-      // Simple config reading for opensource parse-server:
-      // 1st null to detect R.xml.config resource id, 2nd null indicates no custom config param
-      //ParsePushConfigReader config = new ParsePushConfigReader(getApplicationContext(), null, null);
-      //
-      //Parse.initialize(new Parse.Configuration.Builder(this)
-      //   .applicationId(config.getAppId())
-      //   .server(config.getServerUrl()) // The trailing slash is important, e.g., https://mydomain.com:1337/parse/
-      //   .build()
-      //);
+            // Simple config reading for opensource parse-server:
+            // 1st null to detect R.xml.config resource id, 2nd null indicates no custom config param
+            //ParsePushConfigReader config = new ParsePushConfigReader(getApplicationContext(), null, null);
 
-      //
-      // Support parse.com and opensource parse-server
-      // 1st null to detect R.xml.config
-      ParsePushConfigReader config = new ParsePushConfigReader(getApplicationContext(), null,
-          new String[] { "ParseClientKey" });
-      if (config.getServerUrl().equalsIgnoreCase("PARSE_DOT_COM")) {
-        //
-        //initialize for use with legacy parse.com
-        Parse.initialize(this, config.getAppId(), config.getClientKey());
-      } else {
-        Log.d(LOGTAG, "ServerUrl " + config.getServerUrl());
-        Log.d(LOGTAG, "NOTE: The trailing slash is important, e.g., https://mydomain.com:1337/parse/");
-        Log.d(LOGTAG, "NOTE: Set the clientKey if your server requires it, otherwise it can be null");
-        //
-        // initialize for use with opensource parse-server
-        Parse.initialize(new Parse.Configuration.Builder(this).applicationId(config.getAppId())
-            .server(config.getServerUrl()).clientKey(config.getClientKey()).build());
-      }
+            ParsePushConfigReader config = new ParsePushConfigReader(
+                getApplicationContext(), null, new String[] { "ParseClientKey" }
+            );
 
-      Log.d(LOGTAG, "Saving Installation in background");
-      //
-      // save installation. Parse.Push will need this to push to the correct device
-      ParseInstallation.getCurrentInstallation().saveInBackground(new SaveCallback() {
-        @Override
-        public void done(ParseException ex) {
-          if (null != ex) {
+            Log.d(LOGTAG, "ServerUrl " + config.getServerUrl());
+            Log.d(LOGTAG, "NOTE: The trailing slash is important, e.g., https://mydomain.com:1337/parse/");
+            FirebaseApp.initializeApp(this);
+            FirebaseMessaging.getInstance().subscribeToTopic("android");
+            FirebaseMessaging.getInstance().subscribeToTopic("all");
+            Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId(config.getAppId())
+                .server(config.getServerUrl())
+                .build());
+
+            Log.d(LOGTAG, "Saving Installation in background");
+
+            ParseInstallation.getCurrentInstallation().saveInBackground(new SaveCallback() {
+                @Override
+                public void done(ParseException ex) {
+                    if (null != ex) {
+                        Log.e(LOGTAG, ex.toString());
+                    } else {
+                        Log.d(LOGTAG, "Installation saved");
+
+                        String deviceToken = ParseInstallation.getCurrentInstallation().getString("deviceToken");
+
+                        if (deviceToken == null) {
+                            FirebaseInstanceId.getInstance().getInstanceId()
+                                .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<InstanceIdResult> task) {
+
+                                        if (!task.isSuccessful()) {
+                                            return;
+                                        }
+
+                                        // Get new Instance ID token
+                                        String token = task.getResult().getToken();
+                                        Log.i(LOGTAG, "\tToken: " + token);
+                                        ParseFCM.register(token);
+                                    }
+                                });
+                        }
+
+                    }
+                }
+            });
+
+        } catch (ParsePushConfigException ex) {
             Log.e(LOGTAG, ex.toString());
-          } else {
-            Log.d(LOGTAG, "Installation saved");
-          }
         }
-      });
-
-    } catch (ParsePushConfigException ex) {
-      Log.e(LOGTAG, ex.toString());
     }
-  }
 }
